69e180daae43909159b2564bdc60096b6783bb16
JackMarx <seriousfools@gmail.com>
2017-05-03 20:47:00 -0500

    url segment

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,5 +1,51 @@
 class ParamsExamplesController < ApplicationController
   def query
     @message = params["my_message"]
+    @message_2 = params["other_message"]
+  end
+
+  def url_segment
+    @message = params["this_is_a_key"]
+  end
+
+
+
+
+
+
+
+
+
+
+
+  def name
+    if params["title"]
+      @name = params["title"].upcase
+
+      if @name.first.upcase == "A"
+        @message = "mcface"
+      end
+    end
+  end
+
+  def guess
+    @user_guess = params["submission"].to_i
+    winning_number = 42
+
+    if winning_number < @user_guess
+      @answer_message = "too high."
+    elsif winning_number > @user_guess
+      @answer_message = "too low."
+    else 
+      @answer_message = "right on the money!!!"
+    end
   end
 end
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   get '/query_params' => 'params_examples#query'
+  get '/url_segment_params/:this_is_a_key' => 'params_examples#url_segment'
+
+
+
+
+
+
+
+
+
+  get '/name' => 'params_examples#name'
+  get '/guess' => 'params_examples#guess'
 end
+
